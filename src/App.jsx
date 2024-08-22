cc3054612cd74dac4e386bf6844f6745c9b44750
Alexis Martinez <jhonyalexis100@gmail.com>
2024-08-22 18:46:06 -0500

    config styles for the testimony component and add props

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import './App.css';
 import Testimony from './components/Testimony';
 
 
+
 function App() {
 
   return (
     <div className='App'>
       <div className='main-container'>
         <h1>Esto es lo que dicen nuestros alumnos sobre freeCodeCamp:</h1>
-        <Testimony />
+        <Testimony
+          name='Emma Bostian'
+          country='Suecia'
+          image='emma'
+          charge='Ingeniera de Software'
+          company='Spotify'
+          testimony='Siempre he tenido problemas para aprender JavaScript. He tomado muchos cursos, pero el curso de freeCodeCamp fue el que se quedó. Estudiar JavaScript, así como estructuras de datos y algoritmos en freeCodeCamp me dio las habilidades y la confianza que necesitaba para conseguir el trabajo de mis sueños como ingeniero de software en Spotify.'
+        />
       </div>
     </div>
   )
--- src/components/Testimony.jsx
+++ src/components/Testimony.jsx
@@ -1,17 +1,31 @@
-import React  from "react";
-import logo from '../images/testimonio-emma.png'
+import React from "react";
+import '../style-sheets/Testimony.css';
+import emma from '../images/testimonio-emma.png';
+import sarah from '../images/testimonio-sarah.png';
+import shawn from '../images/testimonio-shawn.png';
 
-function Testimony() {
+
+
+
+function Testimony(props) {
+
+    const images = {
+        emma: emma,
+        sarah: sarah,
+        shawn: shawn
+    };
+
     return (
-        <div className='container-testimony'>
-            <img 
-            className='img-testimony'
-            src={logo}
-            alt="image-emma" />
+
+        <div className="container-testimony">
+            <img
+                className="img-testimony"
+                src={images[props.image]}
+                alt="image-emma" />
             <div className="container-text-testimony">
-                <p className="name-testimony">Emma Bostian En Suecia</p>
-                <p className="charge-testimony">Ingeniera De Software En Spotify</p>
-                <p className="text-testimony">Gran Academia, aqui pude aprender JavaScript</p>
+                <p className="name-testimony">{props.name} En {props.country}</p>
+                <p className="charge-testimony">{props.charge} En {props.company}</p>
+                <p className="text-testimony">"{props.testimony}"</p>
 
             </div>
 
